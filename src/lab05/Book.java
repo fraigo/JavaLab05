69ff286c56f7108ff61941fd6a62ea74da999155
Francisco Igor <franciscoigor@gmail.com>
2018-04-20 19:43:56 -0700

    first commit

---
+++ src/lab05/Book.java
@@ -0,0 +1,26 @@
+package lab05;
+
+/**
+ * Book class
+ * @author franciscoigor@gmail.com
+ *
+ */
+public class Book {
+	
+	Name firstName;
+	Name lastName;
+	String title;
+	int yearPublished;
+	
+	public Book(Name firstName, Name lastName, String title, int yearPublished) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.title = title;
+		this.yearPublished = yearPublished;
+	}
+	
+	
+	
+	
+
+}
